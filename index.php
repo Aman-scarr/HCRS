73900ea8bb85be568bd97b1a359c0b04d61c26af
sena jaleta <jaletasena17641@gmail.com>
2021-04-24 18:14:37 +0300

    index.php

--- index.php
+++ index.php
@@ -18,6 +18,44 @@ try{
  echo 'Error';
     
 }
+function getpost()
+{
+    $posts = array();
+    $posts[0] = $_POST['id'];
+    $posts[1] = $_POST['fname'];
+    $posts[2] = $_POST['lname'];
+    $posts[3] = $_POST['age'];
+    return $posts;
+}
+
+//search
+if(isset($_POST['search']))
+{
+    $data = getposts();
+
+    $search_Query = "SELECT * FROM users WHERE id = $data[0]";
+    $search_Result = mysquli_query($connect, $search_Query);
+    if($search_Result)
+    {
+        if(mysqli_num_rows($search_Result))
+        {
+            while($row = mysquli_fetch_array($search_Result))
+            {
+                $id = $row['id'];
+                $fname = $row['fname'];
+                $lname = $row['lname'];
+                $age = $row['age'];
+    
+    
+            }
+        }else{
+            echo 'No Data For This Id';
+        }
+    }else{
+        echo 'Result Error';
+
+    }
+}
 
 ?>
 <!DOCTYPE Haml>
@@ -30,9 +68,12 @@ try{
                <input type="number" name="id" placeholder="Id" value="<?php echo $id;?>"><br><br>
                <input type="text" name="fname" placeholder="First Name"value="<?php echo $fname;?>"><br><br>
                <input type="text" name="lname" placeholder="Last Name"value="<?php echo $lname;?>"><br><br>
-               <input type="number" name="id" placeholder="Age"value="<?php echo $age;?>"><br><br>
+               <input type="number" name="age" placeholder="Age"value="<?php echo $age;?>"><br><br>
              <div>
+                 <input type="submit" name="insert" value="Add">
                  <input type="submit" name="update" value="Update">
+                 <input type="submit" name="delete" value="Delete">
+                 <input type="submit" name="search" value="Find">
                  
             </from>
         </body>
